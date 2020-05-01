fb9c8f2e4ac236407433650acdfdcbe0f9af80b1
Simonexc <30417257+Simonexc@users.noreply.github.com>
2020-05-01 18:53:36 +0200

    Update Turing.py
    Updated check_input_data
    Updated move_head

--- Turing.py
+++ Turing.py
@@ -12,21 +12,45 @@ class TuringMachine:
 
     def check_input_data(self):
 
+        if not type(self.alphabet) is list:
+            raise RuntimeError("Alphabet is not a list")
+
+        if not type(self.state_list) is list:
+            raise RuntimeError("State List is not a list")
+
+        if not type(self.tape) is list:
+            raise RuntimeError("Tape is not a list")
+
         # alphabet should have at least one symbol
-        if len(self.alphabet) == 0:
+        if self.alphabet:
             raise RuntimeError("Insufficient number of symbols")
 
         # state_list should have at least one state
-        if len(self.state_list) == 0:
+        if self.state_list:
             raise RuntimeError("Insufficient number of states")
+
+        # tape should have at least one cell
+        if self.tape:
+            raise RuntimeError("Insufficient number of cells")
 
         # check tape
         for cell in self.tape:
             if cell not in self.alphabet:
                 raise RuntimeError("Unrecognized character on the tape")
-        
-    def move_head(self, direction: chr):
-        pass
+
+    def move_head(self, direction: chr):  # direction: L -> left, R -> right
+        if direction == "L":
+            # add empty symbols if at the start
+            if self.head_position == 0:
+                self.tape.insert(0, self.alphabet[0])
+            else:
+                self.head_position -= 1
+
+        elif direction == "R":
+            self.head_position += 1
+            # add empty symbols if at the end
+            if self.head_position == len(self.tape):
+                self.tape.append(self.alphabet[0])
 
     def perform_operation(self, instruction: list):
         pass
