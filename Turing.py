f58037efdf2c84d5f66ab6c5566e5bbd025a9404
Simonexc <30417257+Simonexc@users.noreply.github.com>
2020-05-01 18:23:32 +0200

    Update Turing.py
    updated __init__

--- Turing.py
+++ Turing.py
@@ -1,6 +1,14 @@
 class TuringMachine:
-    def __init__(self):
-        pass
+    def __init__(self, alphabet, tape, state_list):
+        self.alphabet = alphabet
+        self.tape = tape
+        self.state_list = state_list
+
+        self.state_diagram = {}
+        self.head_position = 0
+        self.current_state = 0
+
+        self.check_input_data()
 
     def check_input_data(self):
         pass
