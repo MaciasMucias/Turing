d4b2559c97080a644245bbe712b66650e468b746
MaciasMucias <unconfigured@null.spigotmc.org>
2020-05-01 18:03:21 +0200

    Added TuringMachine class in Turing.py

---
+++ Turing.py
@@ -0,0 +1,12 @@
+class TuringMachine:
+    def __init__(self):
+        pass
+
+    def check_input_data(self):
+        pass
+
+    def move_head(self, direction: chr):
+        pass
+
+    def perform_operation(self, instruction: list):
+        pass
