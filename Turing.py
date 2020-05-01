408403992c0f91e1cf243a925af2cd7d93b34b76
Simonexc <30417257+Simonexc@users.noreply.github.com>
2020-05-01 18:38:14 +0200

    Update Turing.py
    updated check_input_data

--- Turing.py
+++ Turing.py
@@ -1,8 +1,8 @@
 class TuringMachine:
     def __init__(self, alphabet, tape, state_list):
-        self.alphabet = alphabet
+        self.alphabet = alphabet  # first symbol in alphabet is default empty symbol
         self.tape = tape
-        self.state_list = state_list
+        self.state_list = state_list  # first state is the default starting state
 
         self.state_diagram = {}
         self.head_position = 0
@@ -11,8 +11,20 @@ class TuringMachine:
         self.check_input_data()
 
     def check_input_data(self):
-        pass
 
+        # alphabet should have at least one symbol
+        if len(self.alphabet) == 0:
+            raise RuntimeError("Insufficient number of symbols")
+
+        # state_list should have at least one state
+        if len(self.state_list) == 0:
+            raise RuntimeError("Insufficient number of states")
+
+        # check tape
+        for cell in self.tape:
+            if cell not in self.alphabet:
+                raise RuntimeError("Unrecognized character on the tape")
+        
     def move_head(self, direction: chr):
         pass
 
