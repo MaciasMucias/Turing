6c21abaf82551f105b86fd395912945acf6265d4
Simonexc <30417257+Simonexc@users.noreply.github.com>
2020-05-05 11:21:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from kivy.factory import Factory
 Builder.load_string('''
 <FDDButton@Button>:
     size_hint_y: None
-    height: '50dp'
+    height: '70dp'
 
 <FilterDD>:
     auto_dismiss: False
@@ -48,8 +48,8 @@ runTouchApp(Builder.load_string('''
 #:import fdd __main__.fdd
 
 Button:
-    size_hint: None, None
-    pos: 200, 200
+    size_hint: 1/2, 1/10
+    pos_hint: {"x":0.25, "top": 1}
     on_press:
         fdd.open(self)
 '''))
